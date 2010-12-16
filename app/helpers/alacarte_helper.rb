71d891482796f91362bc95e525d730a151de7ca3
Stijn Mathysen <stijn@skylight.be>
2010-12-16 23:01:01 +0100

    made the current element optional (map to controller name by default) made the html in the link html_safe added group options to the group (ul) tag instead of the default html options

--- app/helpers/alacarte_helper.rb
+++ app/helpers/alacarte_helper.rb
@@ -1,6 +1,8 @@
 module AlacarteHelper
   
-  def navigation_for(menu, current_element)
+  def navigation_for(menu, current_element = nil)
+    current_element = controller_name if current_element.blank?
+    
     if current_menu = Alacarte.menus[menu]
       current_menu.build(self)
       
@@ -10,17 +12,17 @@ module AlacarteHelper
   
   def elements_for(menu, current_element)
     result = ''
-
+    
     menu.items.each do |item|
       if item.valid?
         _html_options = item.html_options || {}
         _html_options[:class] = _html_options[:class].to_s.insert(0, 'active ').strip if (current_element == item.as)
       
         _item = case item.type
-          when :link: link_to(item.label, item.path, _html_options)
+          when :link: link_to(item.label.html_safe, item.path, _html_options)
           else content_tag(item.type, item.label, _html_options)
         end
-      
+        
         if item.items.size > 0
           _item << elements_for(item, current_element)
         end
@@ -29,7 +31,7 @@ module AlacarteHelper
       end
     end
     
-    result.blank? ? '' : content_tag(:ul, result.html_safe, menu.html_options)
+    result.blank? ? '' : content_tag(:ul, result.html_safe, menu.group_options)
   end
 
 end
