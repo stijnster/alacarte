201952bcdc9b36b7a63cc83e74534ca2a13f8c53
Stijn Mathysen <stijn@skylight.be>
2010-12-19 13:34:50 +0100

    Feeling bad about not having tests in this gem. So, here we go...

---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require File.dirname(__FILE__) + '/../lib/alacarte.rb'
+
+# Add I18n load_path
+# I18n.load_path = (I18n.load_path << Dir[File.join(File.dirname(__FILE__), 'locales', '*.yml')]).uniq
+ 
+
